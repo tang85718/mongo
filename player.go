0e01e36f6f770b0e9f6b53517cfeec87f10130ee
tangxuyao <daulgas.tang@stringstech.co>
2018-03-09 17:55:53 +0800

    定义数据表常量

--- player.go
+++ player.go
@@ -5,17 +5,25 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+const (
+	DB_ROOT  = "root"
+	C_PLAYER = "player"
+	C_ACTOR  = "actors"
+)
+
 type Player struct {
-	ID         bson.ObjectId `bson:"_id,omitempty"`
-	DisplayID  string        `bson:"id"`
-	Token      string        `bson:"token"`
-	Phone      string        `bson:"phone"`
-	Country    string        `bson:"country"`
-	Province   string        `bson:"province"`
-	City       string        `bson:"city"`
-	Lat        float64       `bson:"lat"`
-	Lng        float64       `bson:"lng"`
-	Birthday   time.Time     `bson:"birthday"`
-	CreateTime time.Time     `bson:"create_time"`
-	UpdateTime time.Time     `bson:"update_time"`
+	ID          bson.ObjectId `bson:"_id,omitempty"`
+	DisplayID   string        `bson:"id"`
+	Token       string        `bson:"token"`
+	Phone       string        `bson:"phone"`
+	Country     string        `bson:"country"`
+	Province    string        `bson:"province"`
+	City        string        `bson:"city"`
+	Lat         float64       `bson:"lat"`
+	Lng         float64       `bson:"lng"`
+	Birthday    time.Time     `bson:"birthday"`
+	CreateTime  time.Time     `bson:"create_time"`
+	UpdateTime  time.Time     `bson:"update_time"`
+	Platform    int           `bson:"platform"` // 1:iOS 2:Android
+	DeviceToken string        `bson:"dev_token"`
 }
